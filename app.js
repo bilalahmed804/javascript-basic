a4af3964094f98fcd5a1327c1020d1dad227f27e
Bilal Ahmed <ba3801534@gmail.com>
2024-02-22 17:16:52 +0500

    java basic solution

---
+++ app.js
@@ -0,0 +1,103 @@
+// chapter 01
+// Q1
+
+// alert( 10)
+// document.write(100)
+// console.log(200)
+
+//Q2
+
+// console.log( 21 )
+// document.write(typeof 100)
+// console.log(typeof 200)
+
+//Q3
+
+// alert(typeof false)
+// document.write(typeof true)
+// console.log(typeof false)
+
+
+// Q4
+
+// alert(typeof false)
+// alert(typeof "Hello wOrld")
+// document.write(typeof "99")
+// console.log(typeof "Hello wOrld")
+
+// Q5
+
+// var a = "Bilal";
+// var b = "Ahmed"; 
+// document.write("<i><b>Hellow ifhg</b> </i>", a ," <br /> " , b)
+// Chapter 02
+
+// Q1.
+
+// alert("Welcome to js land \n Happy coding..." );
+
+//Q2
+
+// document.write("Welcome to js land <br /> Happy coding..." );
+
+//  Q3.      
+
+// var firstname = "bilal";
+// var lastname = "ahmed";
+
+// var age = 20;
+
+// alert(firstname + "-" + lastname + age)
+
+// Q4. 
+
+// var email = "demo@gamil.com"
+
+// var cnic = 8574658767
+
+// console.log("My" +"  "+ email +"\n My CNIC number"+"  "+ cnic )
+
+// document.write("My" +"  "+ email +"<br /> My CNIC number"+"  "+ cnic )
+
+
+
+// Chapter 03
+//Q1
+// var x = "I am trying to learn from book"
+
+// var y = "a smarter \n way to learn javascritp"
+
+// alert( x + y)
+
+// console.log( x + y)
+
+// Q2
+//  var caseQty = 72
+
+//  var caseQty2 = 72
+
+//  console.log(caseQty + caseQty2)
+
+// Q3
+// var  mystring = "Hello wOrld"
+// alert(mystring)
+// document.write(mystring)
+// console.log(mystring)
+
+// Q4
+
+// var x = 80
+// var y = 70
+
+// var caseQty = x + y
+
+// alert(caseQty)   
+
+// Q5
+// var num = 9
+// var num2 = num*9
+
+// alert(num2)
+
+
+//Chapter complete
