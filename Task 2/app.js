26379b0e6e66c60fd2e7f425687f6acecab78e0a
Bilal Ahmed <ba3801534@gmail.com>
2024-02-26 12:09:50 +0500

    file update

---
+++ Task 2/app.js
@@ -0,0 +1,138 @@
+//                                                Chapter 04
+// Q1.
+var x =10, y = "Hello WOrld", z = true
+
+document.write(x,"<br />",y,"<br />",z)
+alert(x+ "\n" + y + "\n"+ z)
+console.log(x  , y  , z)
+//Q2.
+var myvariable = 20 
+var _variable = "Hello wOrld"
+var $variable = false
+var variable123 = true
+var my_variable = 9*8
+alert(myvariable+"\n"+ _variable +"\n" + $variable + "\n "+ my_variable)
+console.log(myvariable+"\n"+ _variable +"\n" + $variable + "\n" +variable123)
+
+var number;
+console.log(number)
+
+var number = 20;
+var number1 = 20;
+
+var number02 = 40;
+console.log(number + number02)
+
+document.write("<select><option>Bilal</option> <option>Ahmed</option></select>")
+var number =20;
+var $my_1stname = number + 8 + (6 * 8) /4;
+
+console.log($my_1stname)
+ $my_1stname = 76;
+ console.log($my_1stname)
+
+//                                        legal name:
+// Q3.
+var hello = 65;
+var _xyz = 76;
+var $work = 34;
+var _1st_user = 46;
+var i_3927 = "Hello";
+
+
+console.log(hello+"\n"+_xyz+"\n"+$work+" "+_1st_user+"\n"+ i_3927);
+
+                                    // illegal name:
+
+var break = 65;
+var for = 30;
+var else = 67;
+var case = 84;
+
+                // CHAPTER 05  eXPERESSION
+
+// Q1.
+
+var $work_45 = 50;
+var my_variable1 = 40;
+console.log("Sum of"+$work_45+"and",my_variable1)
+
+// camel case
+var firstName = "<h1>Bilal</h1>"
+var lastName = "<i>Ahmed<i>"
+document.write(firstName+lastName)
+
+// Q2.
+Subtraction
+var $number = 6-8;
+alert($number)
+
+var a = 7 , z =4;
+var num = a/z;
+document.write("Division" , num )
+
+var a = 7 , z =4;
+var num$ = a*z;
+document.write("multiplication" , num$ )
+
+// Declare
+// a
+var $_number;
+alert($_number)
+
+// b).
+// intialize value
+var my_1st = 18;
+var my_408 = 2;
+console.log(my_1st * my_408)
+
+// Show the value of variable in your browser like “Initial value: 5”
+
+
+var number09 = 15 ,number_$ =3
+
+console.log(number09 / number_$)
+
+// c)
+// and modulus
+
+var modulus = 25;
+var modulus_$ =500;
+
+alert(modulus % modulus_$)
+
+
+// e).Increment the variable.
+
+var age = 12;
+
+
+console.log(age++ + --age);
+
+
+alert(newage);
+
+var num = 5;
+
+var my_ = --num + ++num - num++ + num + ++num + ++num + --num - --num - num - ++num - ++num + --num;
+  
+console.log(my_);
+
+++num;
+console.log(num)
+
+var num = 5;
+var mynum = --num  + ++num + --num + ++num - --num + ++num;
+console.log(mynum)
+
+ //Q5
+
+var C=30 ,F=80
+document.write(F+"<sup>0</sup>F is"+"  "+((F-32)*5/9)+"<sup>0</sup>C <br>"+
+C+"<sup>0</sup>C is"+" "+((C*9/5)+32)+"<sup>0</sup>F<br><br>")
+
+
+// Q-6
+var a="price of item 1 is" , b="price of item 2 is" , c="Ordered quantity of item 1 is" , d="Ordered quantity of item 2 is" , e="Shipping charges"
+, f=250 , g=150 , h=3 , i=5 , j=120 , num= f*h+g*i+j
+document.write(a+" "+f+"<br>"+b+" "+g+"<br>"+c+" "+h+"<br>"+d+" "+i+"<br>"+e+" "+j+"<br><br>"+"Total Cost of Ordered is"+" "+num+"<br><br>")
