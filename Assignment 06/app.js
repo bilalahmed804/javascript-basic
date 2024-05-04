c9ef85ec7a5326ae52373f328438707d0882f464
Bilal Ahmed <ba3801534@gmail.com>
2024-05-04 20:25:35 +0500

    Alert & Variable String & Variable Number

---
+++ Assignment 06/app.js
@@ -0,0 +1,95 @@
+// Chapter 01 (Alerts) Words file
+
+        //Q1
+alert("First Name : Bilal")
+alert("Last Name : Ahmed")
+alert("Email : bilal@gmail.com")
+alert("Phone Number :948545478294")
+alert("Password : jhjvhufdg$%")
+//Q2
+alert("You're learning javaScript!")
+//Q3 
+alert("I liked javaScript")
+// Chapter 02 (Variables for String)
+//Q1
+var firstName;
+//Q2
+var last_Name = "Ahmed"
+//Q3
+var myTeamName = "Pakistan"
+alert(myTeamName)
+//Q4
+var bestMan = "Charlie"
+// Chapter 3 (Variables for number)
+//Q1
+var caseQty;
+//Q2
+caseQty = 144
+//Q3
+var num = "9"
+//Q4
+var sum = 5 + 8;
+//Q5
+var mechTotal = 100;
+var shippingCharge = 10;
+var orderTotal = mechTotal+ shippingCharge;
+alert(orderTotal);
+//Q6
+var sum = 5 + 8 ;
+var firstNumber = 55;
+var secondNumber = 77;
+var totalNumber = sum + firstNumber + secondNumber;
+alert(totalNumber)
+
+ // chapter 01(alert) pdf file 
+//Q1
+var msg = "Error! Please enter a valid password"
+//Q2
+alert(msg)
+//Q3
+var masge = "Welcome to js Land..."
+var second = "Happy Coding!"
+alert(masge+"\n"+second)
+
+ //Q4
+var msgeNext = "Welcome to JS Land..."
+alert(msgeNext)
+//
+var hppy = "Happy Coding!"
+alert(hppy)
+//Q5
+var sole = "Hello..I can run JS through my web browser's console";
+console.log(sole);
+//Q6
+// chapter 02 ( variable for string)
+//Q1
+var username;
+//Q2
+var myName = "Bilal Ahmed"
+//Q3
+var mesge = "Hello wOrld"
+alert(mesge)
+//Q4
+var names = "Jhone Doe "
+var age = "15 years old "
+var cert = "Certified Mobile Application Development"
+alert(names + age + cert)
+//Q5
+var ab = "PIZZA"
+var bc = "PIZZ"
+var cd = "PIZ"
+var ef = "PI"
+var go = "P"
+alert(ab+"\n"+bc+"\n"+cd+"\n"+ef+"\n"+go);
+//Q6
+var email = "My email address is example@example.com"
+alert(email)
+//Q7
+var learn = "I am trying to learn from the Book \n A smarter way to learn javaScript";
+alert(learn)
+//Q8
+var write = "Yah! I can write HTML content through javaScript"
+document.write(write)
+//Q9
+var show = "“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”"
+alert(show)
